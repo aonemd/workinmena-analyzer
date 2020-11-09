5d7ad7f61cf303dd78ff0d7859ede6fc73df73e4
aonemd <aonemdsaleh@gmail.com>
2020-11-09 20:48:41 +0200

    Add production mode with SSL support

--- main.go
+++ main.go
@@ -10,15 +10,15 @@ import (
 )
 
 func analyze(url string) (tools []string, err error) {
-  appsFile, _ := os.Open("apps.json")
-  wa, _ := webanalyze.NewWebAnalyzer(appsFile, nil)
+	appsFile, _ := os.Open("apps.json")
+	wa, _ := webanalyze.NewWebAnalyzer(appsFile, nil)
 
-  job := webanalyze.NewOnlineJob(url, "", nil, 0, false)
-  result, _ := wa.Process(job)
+	job := webanalyze.NewOnlineJob(url, "", nil, 0, false)
+	result, _ := wa.Process(job)
 
-  for _, a := range result.Matches {
-    tools = append(tools, a.AppName)
-  }
+	for _, a := range result.Matches {
+		tools = append(tools, a.AppName)
+	}
 
 	return
 }
@@ -49,12 +49,12 @@ func handler(rw http.ResponseWriter, req *http.Request) {
 }
 
 func init() {
-  err := webanalyze.DownloadFile(webanalyze.WappalyzerURL, "apps.json")
-  if err != nil {
-    log.Fatalf("error: can not update apps file: %v", err)
-  }
+	err := webanalyze.DownloadFile(webanalyze.WappalyzerURL, "apps.json")
+	if err != nil {
+		log.Fatalf("error: can not update apps file: %v", err)
+	}
 
-  log.Println("app definition file updated from ", webanalyze.WappalyzerURL)
+	log.Println("app definition file updated from ", webanalyze.WappalyzerURL)
 }
 
 func main() {
@@ -63,6 +63,23 @@ func main() {
 		port = "3001"
 	}
 
+	mode := os.Getenv("mode")
+	if mode == "" {
+		mode = "development"
+	}
+
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+	if mode == "production" {
+		log.Fatal(
+			http.ListenAndServeTLS(
+				":"+port,
+				"/etc/letsencrypt/live/workinmena.tech/fullchain.pem",
+				"/etc/letsencrypt/live/workinmena.tech/privkey.pem",
+				nil,
+			),
+		)
+	} else {
+		log.Fatal(http.ListenAndServe(":"+port, nil))
+	}
 }
